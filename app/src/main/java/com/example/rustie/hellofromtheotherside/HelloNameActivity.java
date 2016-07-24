cd57c98541bcee489f4de80b6ecc2c87960aa26b
rustielin <rustie117@gmail.com>
2016-07-23 17:43:10 -0700

    firebase

---
+++ app/src/main/java/com/example/rustie/hellofromtheotherside/HelloNameActivity.java
@@ -0,0 +1,28 @@
+package com.example.rustie.hellofromtheotherside;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HelloNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_name);
+
+        Button name_button = (Button)findViewById(R.id.name_button);
+
+        name_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent();
+                i.setClass(HelloNameActivity.this, MapsActivity.class);
+                startActivity(i);
+                overridePendingTransition(R.anim.rightin, R.anim.leftout);
+            }
+        });
+    }
+}
